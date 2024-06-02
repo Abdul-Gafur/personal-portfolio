0eae26320fd3fdb10f6c487b95392c4c7a55df4c
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-06-02 16:18:03 +0000

    breeze starter kit used to make authentication

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* ====================================================
+****************** FRONTEND ROUTES ********************
+=====================================================*/
+
+Route::get('/', 'App\Http\Controllers\Frontend\FrontendController@index')->name('homepage');
