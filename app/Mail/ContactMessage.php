249871645943d9ebd1122d926876e53ef744a8ea
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-08-14 00:30:29 +0000

    mail added

---
+++ app/Mail/ContactMessage.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMessage extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $data;
+    public $subject;
+    public $attachment;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data,$attachment_list,$subject)
+    {
+        $this->data = $data;
+        $this->subject = $subject;
+        $this->attachment = $attachment_list;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $mail =   $this->from('abdulgafurshaattir@gmail.com', 'My Portfolio Website')
+        ->subject($this->subject)
+        ->view('mail.contact');
+
+        if (!empty($this->attachment)){
+            foreach ($this->attachment as $field_name => $attached_file){
+                if (file_exists($attached_file)){
+                    $mail->attach($attached_file);
+                }
+            }
+        }
+
+        return $mail;
+    }
+}
